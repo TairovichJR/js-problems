7caaab90344b3dfa1120d2209bae530eb047d4fb
TairovichJR <yokubov.omonjon@gmail.com>
2021-01-27 16:51:22 -0500

    positive sums ehanced with reduce

--- script.js
+++ script.js
@@ -120,9 +120,10 @@ const flip = (d, a) => {
 
 // Sum of positive
 function positiveSum(arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i] > 0 ? arr[i] : 0;
-  }
-  return sum;
+  //   let sum = 0;
+  //   for (let i = 0; i < arr.length; i++) {
+  //     sum += arr[i] >= 0 ? arr[i] : 0;
+  //   }
+  //   return sum;
+  return arr.filter((x) => x >= 0).reduce((acc, cur) => acc + cur);
 }
