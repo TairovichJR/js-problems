6656851c8567a04d3982c3095217c597f252d4c0
TairovichJR <yokubov.omonjon@gmail.com>
2021-01-27 14:27:29 -0500

    grade book

--- script.js
+++ script.js
@@ -40,3 +40,21 @@ const quarterOf = (month) => {
 */
   return Math.ceil(month / 3);
 };
+
+//Grasshopper - Grade book
+function getGrade(s1, s2, s3) {
+  const avg = (s1 + s2 + s3) / 3;
+  if (avg >= 90 && avg <= 100) {
+    return "A";
+  } else if (avg >= 80 && avg < 90) {
+    return "B";
+  } else if (avg >= 70 && avg < 80) {
+    return "C";
+  } else if (avg >= 60 && avg < 70) {
+    return "D";
+  } else if (avg >= 0 && avg < 60) {
+    return "F";
+  } else {
+    return "invalid number";
+  }
+}
