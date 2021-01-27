4dadc8bc1332141e7d0bd712c1e45af3d54c2835
TairovichJR <yokubov.omonjon@gmail.com>
2021-01-27 14:18:28 -0500

    quaterly

--- script.js
+++ script.js
@@ -23,3 +23,17 @@ console.log(findDifference([2, 2, 3], [5, 4, 1]));
 var replaceDots = function (str) {
   return str.replace(/\./g, "-");
 };
+
+// Quarter of the year
+const quarterOf = (month) => {
+  // Your code here
+  if (month >= 1 && month <= 3) {
+    return 1;
+  } else if (month >= 4 && month <= 6) {
+    return 2;
+  } else if (month >= 7 && month <= 9) {
+    return 3;
+  } else if (month >= 10 && month <= 12) {
+    return 4;
+  }
+};
